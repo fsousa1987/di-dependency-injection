a2aa469b5b7cf0a66c3bbfb31413c8b0599eb818
Francisco Sousa <franciscoeds1987@gmail.com>
2021-01-29 14:28:14 -0300

    O que é injeção de dependências?

---
+++ src/com/algaworks/di/modelo/Produto.java
@@ -0,0 +1,23 @@
+package com.algaworks.di.modelo;
+
+import java.math.BigDecimal;
+
+public class Produto {
+
+    private final String nome;
+    private final BigDecimal valorTotal;
+
+    public Produto(String nome, BigDecimal valorTotal) {
+        super();
+        this.nome = nome;
+        this.valorTotal = valorTotal;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+}
